36433cbecb11d6934307b81faf01b1da260042bf
sacha236 <109908201+sacha236@users.noreply.github.com>
2022-07-25 21:44:18 +0700

    Update mtm.sh

--- mintme/mtm.sh
+++ mintme/mtm.sh
@@ -1,6 +1,4 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.2/SRBMiner-Multi-1-0-2-Linux.tar.xz
-tar -xvf SRBMiner-Multi-1-0-2-Linux.tar.xz
-cd SRBMiner-Multi-1-0-2
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.2/SRBMiner-Multi-1-0-2-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-2-Linux.tar.xz && cd SRBMiner-Multi-1-0-2
 rm -rf mintme.sh
 wget -O mintme.sh https://raw.githubusercontent.com/sacha236/awe/main/mintme/mintme.sh
 ./mintme.sh
