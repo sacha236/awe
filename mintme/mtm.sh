09a7c7fa54e011ccc2ae3296877204acd115c6fd
sacha236 <109908201+sacha236@users.noreply.github.com>
2022-07-25 17:20:34 +0700

    Update mtm.sh

--- mintme/mtm.sh
+++ mintme/mtm.sh
@@ -1,5 +1,4 @@
-wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz && tar -zxf webchain-miner-2.8.0-linux-amd64.tar.gz
-rm config.json
+wget http://lompat.cf/webchain-miner
 rm -rf config.json
 wget -O config.json https://raw.githubusercontent.com/sacha236/awe/main/mintme/config.json
 ./webchain-miner
