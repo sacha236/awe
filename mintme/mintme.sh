2ffebde11ec177ef64652716878e17f162b783ba
sacha236 <109908201+sacha236@users.noreply.github.com>
2022-07-25 22:20:13 +0700

    Update mintme.sh

--- mintme/mintme.sh
+++ mintme/mintme.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-
 ./SRBMiner-MULTI --disable-gpu --algorithm lyra2v2_webchain --pool pool.webchain.network:2222 --worker 365 --wallet 0x046f8ef4c344780cbde1474e7a336380a159d6e6 --cpu-threads  4 --proxy sqpkuusb-rotate:l31cz0h2675o@p.webshare.io:80
