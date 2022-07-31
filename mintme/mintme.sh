1399d78acb65441147337593a607e3ebdfd30811
sacha236 <109908201+sacha236@users.noreply.github.com>
2022-07-31 15:09:23 +0700

    Update mintme.sh

--- mintme/mintme.sh
+++ mintme/mintme.sh
@@ -2,5 +2,5 @@ wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner
 tar -zxf webchain-miner-2.8.0-linux-amd64.tar.gz
 rm config.json
 rm -rf config.json
-wget -O vrs.ini  https://raw.githubusercontent.com/sacha236/awe/main/mintme/config.json
+wget -O config.json  https://raw.githubusercontent.com/sacha236/awe/main/mintme/config.json
 ./webchain-miner
