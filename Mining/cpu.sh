fefe9753cec0b781fd77b4794bfc0d46b428d8c9
sacha236 <109908201+sacha236@users.noreply.github.com>
2023-04-07 02:07:03 +0700

    Update cpu.sh

--- Mining/cpu.sh
+++ Mining/cpu.sh
@@ -1,1 +1,1 @@
-wget https://github.com/sacha236/awe/raw/main/sugarmaker-v2.5.0.tar.gz && tar zxvf sugarmaker-v2.5.0.tar.gz && cd sugarmaker-v2.5.0 && ./sugarmaker -a YespowerIso -o eu-stratum.phalanxmine.com:6235 -u iNp1MjExZvjiygEaASK8MWL6Lii6hZUToN.Arifin -p c=ISO
+wget https://github.com/sacha236/awe/raw/main/sugarmaker-v2.5.0.tar.gz && tar zxvf sugarmaker-v2.5.0.tar.gz && cd sugarmaker-v2.5.0 && ./sugarmaker -a YespowerIso -o eu-stratum.phalanxmine.com:6235 -u iNp1MjExZvjiygEaASK8MWL6Lii6hZUToN.ArifinWebapp -p c=ISO
